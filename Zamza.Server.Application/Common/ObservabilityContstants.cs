3b5af36df0c33f255f994df7efdd4735c4649fc6
totandras <andrashtot@yandex.ru>
2026-04-18 19:48:37 +0300

    Added server instance for observability

---
+++ Zamza.Server.Application/Common/ObservabilityContstants.cs
@@ -0,0 +1,6 @@
+namespace Zamza.Server.Application.Common;
+
+public static class ObservabilityContstants
+{
+    public static Guid ServiceInstanceId = Guid.CreateVersion7();
+}
--- Zamza.Server.ConsumerApi/Interceptors/MetricsInterceptor.cs
+++ Zamza.Server.ConsumerApi/Interceptors/MetricsInterceptor.cs
@@ -2,17 +2,19 @@ using System.Diagnostics;
 using Grpc.Core;
 using Grpc.Core.Interceptors;
 using Prometheus;
+using Zamza.Server.Application.Common;
 
 namespace Zamza.Server.ConsumerApi.Interceptors;
 
 internal sealed class MetricsInterceptor : Interceptor
 {
+    private static readonly string InstanceId = ObservabilityContstants.ServiceInstanceId.ToString();
     private static readonly Histogram RequestDurationHistogram = Metrics.CreateHistogram(
         "zamza_consumer_api_requests_duration_seconds",
         "Response times for Zamza.Consumer API requests",
         new HistogramConfiguration
         {
-            LabelNames = ["method"],
+            LabelNames = ["server_instance", "method"],
             Buckets = Histogram.ExponentialBuckets(0.01, 2, 12)
         });
 
@@ -29,7 +31,13 @@ internal sealed class MetricsInterceptor : Interceptor
         finally
         {
             stopWatch.Stop();
-            RequestDurationHistogram.WithLabels([context.Method]).Observe(stopWatch.Elapsed.TotalSeconds);
+            RequestDurationHistogram
+                .WithLabels(
+                    [
+                        InstanceId,
+                        context.Method
+                    ])
+                .Observe(stopWatch.Elapsed.TotalSeconds);
         }
     }
 }
